2fc2ac8513dfb736e1efc5a948621e60ade93527
malikireddykarthikreddy <malikireddykr@gmail.com>
2024-10-18 12:52:36 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "2048gamemani" # Replace with your actual S3 bucket name
+    bucket = "2048p" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-south-1"
   }
 }
